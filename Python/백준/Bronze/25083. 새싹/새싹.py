1f95975cc25c7b8a8d8de1de769a025693f3d9fd
hellowooan <ywan.kr@gmail.com>
2026-03-27 00:50:23 +0900

    [Bronze V] Title: 새싹, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+"""
+print()를 쓸 때 특별취급해야할 문자
+- \', \", \\
+- \n : 줄바꿈
+- \t : tab
+"""
+
+print("         ,r\'\"7\n" \
+"r`-_   ,\'  ,/\n" \
+" \\. \". L_r\'\n" \
+"   `~\\/\n" \
+"      |\n" \
+"      |"
+)
